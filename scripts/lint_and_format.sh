48e9aef4cce71610fa085b32b6b986c15d502f17
Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>
2025-12-24 07:22:29 +0545

    add djlint
    Signed-off-by: Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>

---
+++ scripts/lint_and_format.sh
@@ -0,0 +1,1 @@
+djlint templates --reformat --lint --format-css --format-js --profile django --quiet --use-gitignore --format-attribute-template-tags --indent-css 4 --indent-js 4 --max-blank-lines 1
