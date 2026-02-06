5532ce8c441e22adbf28fe025a1c71876071d45e
Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>
2026-02-06 23:04:02 +0545

    static pages, config, and .env-example
    Signed-off-by: Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -1,17 +1,8 @@
 from django.views.generic import TemplateView
 
-from apps.events.models import Event
-
 
 class HomeView(TemplateView):
     template_name = "pages/home.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["featured_events"] = Event.objects.filter(
-            is_approved=True
-        ).select_related("organizer")[:6]
-        return context
 
 
 class AboutView(TemplateView):
